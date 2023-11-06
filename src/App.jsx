731844897a4f88406d819c4f47893ac261ada146
richardsonadjei <adjei.kwadjo@gmail.com>
2023-11-06 12:38:41 +0000

    Create Pages And Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 
-import './App.css'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Header from './components/Header'
+
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
  
 
   return (
-    <div>
-      <Header />
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/sign-in' element={<SignIn />} />
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/profile' element={<Profile />} />
+    </Routes>
+  </BrowserRouter>
   )
 }
 
