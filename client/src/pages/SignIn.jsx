ea7e00f7a8a85620b5578ae977a13abc646be8cf
richardsonadjei <adjei.kwadjo@gmail.com>
2023-11-06 20:15:06 +0000

    Complete SignIn Page Functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,92 @@
-import React from 'react'
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
-export default function SignIn() {
+const SignIn = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        // User successfully signed in
+        alert('User signed in successfully!');
+        window.location.href = '/';
+      } else {
+        // Failed to sign in
+        alert('Failed to sign in. Please check your credentials and try again.');
+      }
+    } catch (error) {
+      console.error('Error signing in:', error);
+      alert('Failed to sign in. Please try again later.');
+    }
+  };
+
   return (
-    <div>SignIn</div>
-  )
-}
+    <div className="container mt-5">
+      <h2 className="mb-4">Sign In</h2>
+      <form onSubmit={handleSubmit}>
+        {/* Email */}
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        {/* Password */}
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        {/* Submit button */}
+        <button type="submit" className="btn btn-primary">
+          Sign In
+        </button>
+
+        {/* Paragraph with SignUp link */}
+        <p className="mt-3">
+          Don't have an account? <a href="/signup">Sign Up</a>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
