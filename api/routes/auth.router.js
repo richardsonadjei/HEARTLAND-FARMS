c74f20a805054ad5c4642c35a2a786281c7c1a76
richardsonadjei <adjei.kwadjo@gmail.com>
2024-03-11 08:48:24 +0000

    m

--- api/routes/auth.router.js
+++ api/routes/auth.router.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import { signin, signout, signup } from '../controllers/auth.controller.js';
-import { verifyAdminToken } from '../utils/verifyAdmin.js';
+
 
 const authRouter = express.Router();
 
-authRouter.post("/signup",verifyAdminToken, signup);
+authRouter.post("/signup", signup);
 authRouter.post("/signin", signin);
 authRouter.post('/signout', signout);
 
